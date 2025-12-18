a3027764d437ca380e5df0fcf1b7a5d744c07921
Wale Salami <wale.salami@globallogic.com>
2025-12-18 13:49:00 +0000

    feat: ollama template

--- templates/ollama/data.tf
+++ templates/ollama/data.tf
@@ -7,7 +7,7 @@ data "coder_parameter" "repo_url" {
   type        = "string"
   name        = "repo_url"
   display_name = "Repo URL"
-  default     = "git@git.globallogic.local:sirlegendary/coder-templates.git"
+  # default     = "git@git.globallogic.local:sirlegendary/coder-templates.git"
   description = "Repo to clone/build."
   mutable     = true
 }
