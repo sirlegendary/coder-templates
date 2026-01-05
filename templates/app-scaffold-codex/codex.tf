9da0f3457a3aeb681138022237408080322aea88
Wale Salami <wale.salami@globallogic.com>
2026-01-05 10:02:58 +0000

    fix: minor

--- templates/app-scaffold-codex/codex.tf
+++ templates/app-scaffold-codex/codex.tf
@@ -3,7 +3,7 @@ module "codex" {
   version        = "3.1.1"
   agent_id       = coder_agent.main.id
   openai_api_key = data.kubernetes_secret_v1.openai_api_key.data["api-key"]
-  codex_model    = "gpt-5-codex"
+  codex_model    = "gpt-5.1-codex-max"
   ai_prompt      = data.coder_task.me.prompt 
   workdir        = local.repo_base_dir
 
